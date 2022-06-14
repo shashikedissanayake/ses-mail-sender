293d34d4a31577e620fc90c9c5f560926c152795
Shashika Chathuranga <shashikedissanayake@gmail.com>
2022-06-14 13:09:22 +0530

    Changed msg send library
    - Included new lib `mimemessage` instead of `nodemailer` to send mails
    - Changed handler to use new library and send emailthrough ses
    - Included sample event

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,34 +1,59 @@
 
 import { SES } from 'aws-sdk';
-import { createTransport } from 'nodemailer';
 import { readFileSync } from 'fs';
+import * as mimemessage from 'mimemessage';
 
 const ses = new SES({ region: 'ap-northeast-2' });
-const transporter = createTransport({ SES: ses })
 
 
 export async function handler(event, _context) {
     console.log(event);
-    const stream = readFileSync('./src/files/text.txt');
-    const mailOptions = {
-        from: event?.from,
-        subject: 'This is an email sent from a Lambda function!',
-        html: `<p>You got a contact message from: </p>`,
-        to: event?.to,
-        attachments: [
-            {
-                filename: "text.txt",
-                content: stream,
-            }
-        ]
-    };
-
-    const id = await new Promise((resolve, reject) => {
-        transporter.sendMail(mailOptions, (error, success) => {
-            if (error) reject(error.message);
-            resolve(success);
-        })
+    const message = mimemessage.factory({
+        contentType: 'multipart/mixed',
+        body: [],
     });
 
-    console.log(`message send success with id ${id}`);
+    message.header('From', `${event.fromName}<${event.from}>`);
+    message.header('To', `${event.to}`);
+    message.header('Subject', 'This is an email sent from a Lambda function!');
+
+    const alternateEntity = mimemessage.factory({
+        contentType: 'multipart/alternate',
+        body: []
+    });
+
+    const htmlEntity = mimemessage.factory({
+        contentType: 'text/html;charset=utf-8',
+        body:  '   <html>  '  + 
+               '   <head></head>  '  + 
+               '   <body>  '  + 
+               '   <h1>Hello!</h1>  '  + 
+               '   <p>Please see the attached file for a list of    customers to contact.</p>  '  + 
+               '   </body>  '  + 
+               '  </html>  ' 
+      });
+     const plainEntity = mimemessage.factory({
+        body: 'Please see the attached file for a list of    customers to contact.'
+     });
+     alternateEntity.body.push(htmlEntity);
+     alternateEntity.body.push(plainEntity);
+     message.body.push(alternateEntity);
+
+    const stream = readFileSync('./src/files/text.txt');
+    const attachmentEntity = mimemessage.factory({
+        contentType: 'text/plain',
+        contentTransferEncoding: 'base64',
+        body: stream.toString('base64').replace(/([^\0]{76})/g, "$1\n")
+    });
+    attachmentEntity.header('Content-Disposition', 'attachment ;filename="text.txt"');
+    message.body.push(attachmentEntity);
+
+    const id = await new Promise((resolve, reject) => {
+        ses.sendRawEmail({ RawMessage: { Data: message.toString() }}, (err, data) => {
+            if (err) reject(err);
+            resolve(data) 
+        });
+    });
+
+    console.log(`message send success with id ${JSON.stringify(id)}`);
 }
